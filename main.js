7eb456265d02dafbafd17d95d14890a3167db6d7
Jon Paul <jonpaul.simonelli@gmail.com>
2014-03-11 02:03:00 -0400

    cleanup

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+/* 
+ * 'Scheduler' by Jon Paul, 2014
+ * Given: a list of classes with meeting days and hours; required courses; schedule preferences
+ * Returns: a collection of possible schedules, with the ability to filter down further
+ */
+
 var name = prompt("What is your Skidmore ID?");
 console.log(name);
